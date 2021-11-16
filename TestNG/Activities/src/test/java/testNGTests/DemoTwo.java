5f0844096e3569a31c059781a7d3b5a380516785
Tirtha Bhattacharya <43333067+tirtha91@users.noreply.github.com>
2021-11-16 13:16:29 +0530

    Add files via upload

---
+++ TestNG/Activities/src/test/java/testNGTests/DemoTwo.java
@@ -0,0 +1,10 @@
+package testNGTests;
+
+import org.testng.annotations.Test;
+
+public class DemoTwo {
+    @Test
+    public void TestCase() {
+        System.out.println("I'm in the test case from DemoTwo Class");
+    }
+}
